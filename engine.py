d584f9b1ec1d1ba47d8f51ef4b4c18af8a2864e6
Mate Papai <papaimate166@gmail.com>
2021-01-04 12:18:13 +0100

    Improved (almost-working) create_board with obstacles

--- engine.py
+++ engine.py
@@ -1,11 +1,25 @@
+import random
+
+
 def create_board(width, height):
     border = "🌳"
     fill = "🎋"
     board = []
+    obstacle = "🗻"
+    obstacle_counter = 0
+    obstacle_max = (width * height) // 100 * 15
     board.append(border * width)
     for i in range(height - 2):
         board.append(border + (fill * (width - 2)) + border)
     board.append(border * width)
+    random_line_index = 0
+    random_row_index = 0
+    while obstacle_counter != obstacle_max:
+        random_line_index = random.randint(1, height - 1)
+        random_row_index = random.randint(1, width - 1)
+        if board[random_line_index][random_row_index] != obstacle:
+            board[random_line_index][random_row_index] = obstacle
+            obstacle_counter += 1
     return board
 
 

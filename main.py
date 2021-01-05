57ab19c9073957e882498b658340153f54883655
Mate Papai <papaimate166@gmail.com>
2021-01-05 12:28:23 +0100

    Added special ability to the playable classes

--- main.py
+++ main.py
@@ -17,27 +17,31 @@ def create_player():
                 "Type": "Warrior",
                 "Health": "10",
                 "Damage": "3",
+                "Special ability": "Pumps 2 times with his chest muscles, creating a resonating wave, dealing 2 extra damage."
     }
     not_crazy_scientist = {
                 "Name": "Cili",
                 "Race": "Consistent female orator",
                 "Type": "Mage",
                 "Health": "7",
-                "Damage": "5"
+                "Damage": "5",
+                "Special ability": "Asks her opponent to only attack if her opponent can win, makes her opponent confused, granting Cili an extra turn"
     }
     pali_gyorfi = {
                 "Name": "Pal Gyorfi",
                 "Race": "Make you stay at home kind of man",
                 "Type": "Archer",
                 "Health": "5",
-                "Damage": "5"
+                "Damage": "5",
+                "Special ability": "Asks his opponent to go home, so Pal can evade the fight."
     }
     op_char = {
                 "Name": "Achilles Lakatos",
                 "Race": "Definitely not c-type",
                 "Type": "Rogue",
                 "Health": "999",
-                "Damage": "999"
+                "Damage": "999",
+                "Special ability": "Makes 3 exact copy of himself during daytime, at nighttime makes 10 copy."
     }
 
 

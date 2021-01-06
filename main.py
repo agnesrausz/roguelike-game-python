bcf667e9ccd837f9a055f5a2664ed258cbb366b1
Mate Papai <papaimate166@gmail.com>
2021-01-06 12:14:26 +0100

    Added option to chose classes in create_player function

--- main.py
+++ main.py
@@ -12,38 +12,68 @@ BOARD_HEIGHT = 20
 
 def create_player():
     male_nurse = {
-                "Name": "Tyson",
-                "Race": "Bodybuilder male-nurse",
-                "Type": "Warrior",
-                "Health": "10",
-                "Damage": "3",
-                "Special ability": "Pumps 2 times with his chest muscles, creating a resonating wave, dealing 2 extra damage."
+                "Name:": "Tyson",
+                "Race:": "Bodybuilder male-nurse",
+                "Type:": "Warrior",
+                "Health:": "10",
+                "Damage:": "3",
+                "Special ability:": "Pumps 2 times with his chest muscles, creating a resonating wave, dealing 2 extra damage."
     }
     not_crazy_scientist = {
-                "Name": "Cili",
-                "Race": "Consistent female orator",
-                "Type": "Mage",
-                "Health": "7",
-                "Damage": "5",
-                "Special ability": "Asks her opponent to only attack if her opponent can win, makes her opponent confused, granting Cili an extra turn"
+                "Name:": "Cili",
+                "Race:": "Consistent female orator",
+                "Type:": "Mage",
+                "Health:": "7",
+                "Damage:": "5",
+                "Special ability:": "Asks her opponent to only attack if her opponent can win, makes her opponent confused, granting Cili an extra turn"
     }
     pali_gyorfi = {
-                "Name": "Pal Gyorfi",
-                "Race": "Makes you stay at home kind of man",
-                "Type": "Archer",
-                "Health": "5",
-                "Damage": "5",
-                "Special ability": "Asks his opponent to go home, so Pal can evade the fight."
+                "Name:": "Pal Gyorfi",
+                "Race:": "Makes you stay at home kind of man",
+                "Type:": "Archer",
+                "Health:": "5",
+                "Damage:": "5",
+                "Special ability:": "Asks his opponent to go home, so Pal can evade the fight."
     }
     op_char = {
-                "Name": "Achilles Lakatos",
-                "Race": "Definitely not c-type",
-                "Type": "Rogue",
-                "Health": "999",
-                "Damage": "999",
-                "Special ability": "Makes 3 exact copy of himself during daytime, at nighttime makes 10 copy."
+                "Name:": "Achilles Lakatos",
+                "Race:": "Definitely not c-type",
+                "Type:": "Rogue",
+                "Health:": "999",
+                "Damage:": "999",
+                "Special ability:": "Makes 3 exact copy of himself during daytime, at nighttime makes 10 copy."
     }
-
+    print("Please select a character!")
+    print("1")
+    for key, value in male_nurse.items():
+        print(key, value)
+    print("\n2")
+    for key, value in not_crazy_scientist.items():
+        print(key, value)
+    print("\n3")
+    for key, value in pali_gyorfi.items():
+        print(key, value)
+    print("\n4 (Can only be chosen If you can enter the correct cheat code!)")
+    for key, value in op_char.items():
+        print(key, value)
+    choice = input("\nEnter the selected character's number(1-4)!")
+    if choice == "1":
+        return male_nurse
+    elif choice == "2":
+        return not_crazy_scientist
+    elif choice == "3":
+        return pali_gyorfi
+    elif choice == "4":
+        cheatcode = ""
+        while cheatcode != "nincs egy cigid?" or cheatcode != "q":
+            cheatcode = input("Provide the correct cheat code to continue, or press q to return!")
+            if cheatcode == "nincs egy cigid?":
+                return op_char
+            if cheatcode == "q":
+                create_player()     
+    else:
+        print("That wasn't an option")
+        create_player()
 
 def main():
     player = create_player()
